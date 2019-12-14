b2ec338497aae92166d30a032dc6ec751b689fb3
Stephen Tafoya <stephen@tafoya.dev>
2019-12-13 21:22:06 -0700

    Implemented jquery drop down

---
+++ www/js/complaint-expander.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.complaints-snitch-button').click(function () {
+        $('.complaints-snitch-contract').slideToggle(1000);
+    });
+});
---
+++ www/js/index-slick.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel-thinker').slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        infinite: true,
+        dots: true,
+        autoplay:true,
+        autoplaySpeed:3000,
+    });
+});
